2408e9f209f95249f362324006309ec4182fcd1b
GuMengYu <m13628414752@163.com>
2019-09-16 17:23:55 +0800

    imporve: add tagsCard component

---
+++ src/components/tagsCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-card
+        class="mx-auto"
+        :flat="true"
+        color="#f0f0f0"
+    >
+        <v-card-title v-text="title"/>
+        <v-divider></v-divider>
+        <div class="subject-list">
+        <v-btn 
+            small 
+            v-for="(subject, index) in subjects"
+            v-text="subject.label"
+            :key="index" 
+            :outlined="true" 
+            :rounded="true"
+        />
+        </div>
+    </v-card>
+</template>
+<script>
+export default {
+    name: 'tags-card',
+    props: {
+        title: '',
+        subjects: {
+            type: Array,
+            default: [],
+        }
+    },
+}
+</script>
+<style lang="less" scoped>
+.subject-list {
+    padding: 5px;
+    button {
+    margin: 0 7px 7px 0;
+    }
+}
+</style>
--- src/views/subPage/index.vue
+++ src/views/subPage/index.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>
-        这是{{$attrs.tab}}子页面哟
-    </div>
+<v-container fluid>
+    <v-row>
+        <v-col lg="8" tag="main">
+            <section>
+                具體的
+            </section>
+        </v-col>
+        <v-col lg="4" tag="aside">
+            <tags-card :title="tag.title" :subjects="tag.subjects"/>
+        </v-col>
+    </v-row>
+</v-container>
 </template>
 <script>
+import TagsCard from '@/components/tagsCard';
 export default {
     name: 'subPage',
+    components: {TagsCard},
     data: () =>({
-
+        tag: {
+            title: '热点主题',
+            subjects: [
+            { label: '程序员', val: 'fun' },
+            { label: 'py', val: 'fun' },
+            { label: '有点东西', val: 'fun' },
+            { label: 'NodeJs', val: 'fun' },
+            { label: 'JAVA', val: 'fun' },
+            { label: 'C++', val: 'fun' },
+            { label: '前端', val: 'fun' },
+            ],
+        }
+        
     }),
     mounted() {
 
