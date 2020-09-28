fe3d8387007a3706fda8342a6ccdacc8c9984406
yuhong <1415515984yuri@gmail.com>
2020-09-28 17:10:20 +0800

    feat(gank/nav/tab): 导航tab 🙊

--- src/views/gank/index.vue
+++ src/views/gank/index.vue
@@ -2,9 +2,9 @@
   <v-container class="gank container" fluid>
     <v-row>
       <v-col lg="8" tag="main">
-        <v-carousel 
-          cycle 
-          delimiter-icon="mdi-minus" 
+        <v-carousel
+          cycle
+          delimiter-icon="mdi-minus"
           hide-delimiter-background
           show-arrows-on-hover
         >
@@ -15,6 +15,17 @@
           >
           </v-carousel-item>
         </v-carousel>
+        <v-tabs>
+          <v-tab v-for="tab in tabs" :key="tab.id">
+            <v-icon :color="tab.color" v-text="tab.icon"/>{{ tab.title }}
+          </v-tab>
+          <v-tab-item
+            v-for="tab in tabs"
+            :key="tab.id"
+          >
+            <page :meta="tab"></page>
+        </v-tab-item>
+        </v-tabs>
       </v-col>
       <v-col lg="4" tag="aside"> </v-col>
     </v-row>
@@ -22,7 +33,9 @@
 </template>
 
 <script>
+import page from './pages';
 export default {
+  components: {page},
   props: {},
   data: () => ({
     slides: [
@@ -44,17 +57,61 @@ export default {
         url: "https://gank.io/post/5e7b5a8b6d2e518fdeab27aa",
       },
     ],
+    tabs: [
+      {
+        title: 'ios',
+        icon: "mdi-apple-ios",
+        id: "ios",
+        color: "#f95e74",
+      },
+      {
+        title: "安卓",
+        icon: "mdi-android",
+        id: "android",
+        color: "#34a853",
+      },
+      {
+        title: "前端",
+        icon: "mdi-react",
+        id: "frontend",
+        color: "#ea4335",
+      },
+      {
+        title: "后端",
+        icon: "mdi-language-java",
+        id: "backend",
+        color: "#ea4335",
+      },
+      {
+        title: "flutter",
+        icon: "mdi-monitor-cellphone",
+        id: "flutter",
+        color: "#ff9a00",
+      },
+      {
+        title: "app",
+        icon: "mdi-whatsapp",
+        id: "app",
+        color: "#ff9a00",
+      },
+      {
+        title: "妹子",
+        icon: "mdi-face-woman-shimmer",
+        val: "girl",
+        color: "pink",
+      },
+    ],
   }),
 };
 </script>
 <style lang="less" scoped>
 .container {
   .v-carousel {
-    @media(max-width: 1736px){
+    @media (max-width: 1736px) {
       width: 740px;
       height: 370px !important;
     }
-    @media(max-width: 1260px){
+    @media (max-width: 1260px) {
       width: 620px;
       height: 310px !important;
     }
---
+++ src/views/gank/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+ <v-container>
+     {{meta.title}}
+ </v-container>
+</template>
+
+<script>
+export default {
+    props: {
+        meta: {
+            type: Object,
+        }
+    },
+    created() {
+        console.log(this.meta.title + 'created');
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style>
+
+</style>
