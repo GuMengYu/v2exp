3b658da3d96e677cf2870d55b805b34a1f8181b2
yuhong <1415515984yuri@gmail.com>
2020-10-01 17:54:32 +0800

    improve(gank): 抽离两种样式的卡片

--- src/views/gank/tab/tab-item.vue
+++ src/views/gank/tab/tab-item.vue
@@ -1,98 +1,20 @@
 <template>
   <div>
     <template v-if="data.type === 'Girl'">
-      <v-card class="mx-auto">
-        <v-img
-          class="white--text align-end"
-          height="300px"
-          :src="data.images[0]"
-        >
-          <v-card-title>{{ data.desc }}</v-card-title>
-        </v-img>
-        <v-card-title class="pt-2 pb-2">
-          <v-chip color="#448ef6" label text-color="white" x-small>妹纸</v-chip>
-          <span class="ml-2">{{ data.title }}</span>
-        </v-card-title>
-        <v-card-text class="pb-0">
-          <v-row justify="space-between" class="ml-0 mr-0">
-            <div>
-              <v-avatar rounded="true" size="16" class="mr-1">
-                <v-img
-                  src="https://ae01.alicdn.com/kf/U0a3fb04845c14bcab78c6e6af1f8fefci.jpg"
-                />
-              </v-avatar>
-              <span class="text-caption">
-                <a :title="`由${data.author}发布`" rel="author">{{
-                  data.author
-                }}</a>
-              </span>
-              <i class="text-caption ml-1 mr-1">-</i>
-              <span class="text-caption">
-                <a href="/Girl" target="_blank">妹纸</a>
-              </span>
-            </div>
-            <div>
-              <span class="text-caption">
-                <time class="mx-1">{{ data.publishedAt }}</time>
-              </span>
-            </div>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="orange" text> Download </v-btn>
-        </v-card-actions>
-      </v-card>
+     <card-image :data="data"></card-image>
     </template>
     <template v-else>
-      <v-card>
-        <v-row justify="space-between" class="ml-0 mr-0">
-          <v-col lg="4">
-            <v-img height="145" :src="data.images[0]"></v-img>
-          </v-col>
-          <v-col lg="8">
-            <v-card-title class="pa-0">
-              <v-chip color="#448ef6" label text-color="white" x-small>{{
-                data.type
-              }}</v-chip>
-              <span class="ml-2">{{ data.title }}</span>
-            </v-card-title>
-            <v-card-text class="pl-0 pt-2 pb-2">
-              <span class="ml-2">{{ data.desc }}</span>
-            </v-card-text>
-            <v-card-text class="pl-0 pt-2 pb-2">
-              <v-row justify="space-between" class="ml-0 mr-0">
-                <div>
-                  <v-avatar rounded="true" size="16" class="mr-1">
-                    <v-img
-                      src="https://ae01.alicdn.com/kf/U0a3fb04845c14bcab78c6e6af1f8fefci.jpg"
-                    />
-                  </v-avatar>
-                  <span class="text-caption">
-                    <a :title="`由${data.author}发布`" rel="author">{{
-                      data.author
-                    }}</a>
-                  </span>
-                  <i class="text-caption ml-1 mr-1">-</i>
-                  <span class="text-caption">
-                    <a href="/Girl" target="_blank">妹纸</a>
-                  </span>
-                </div>
-                <div>
-                  <span class="text-caption">
-                    <time class="mx-1">{{ data.publishedAt }}</time>
-                  </span>
-                </div>
-              </v-row>
-            </v-card-text>
-          </v-col>
-        </v-row>
-      </v-card>
+      <card-plian :data="data"></card-plian>
     </template>
   </div>
 </template>
 
 <script>
+import CardPlian from '@/components/card-plain';
+import CardImage from '@/components/card-image';
+
 export default {
+  components: {CardPlian, CardImage},
   props: {
     data: {
       type: Object,
