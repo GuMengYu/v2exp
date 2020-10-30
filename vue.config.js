9a35dcbccbdb64ac9999bfb91f60e60af12a3c1b
yuhong <1415515984yuri@gmail.com>
2020-10-30 16:29:59 +0800

    build: 打包 chunk 拆分

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,31 @@ module.exports = {
       disableHostCheck: true,
       port: port,
     },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          libs: {
+            name: 'chunk-libs',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: 'initial', // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: 'chunk-VuetifyUI', // split vuetify into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?vuetify(.*)/,
+          },
+          commons: {
+            name: 'chunk-commons',
+            test: path.resolve(__dirname, 'src/components'), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true,
+          },
+        },
+      },
+    },
   },
   outputDir: path.resolve(__dirname, 'docs'),
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
